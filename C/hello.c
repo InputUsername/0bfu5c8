93d6140c8806fa3920b951be4ad97af09ac796ae
InputUsername <InputUsername@users.noreply.github.com>
2015-06-14 23:46:10 +0200

    Last time, I promise
    hello.c

--- C/hello.c
+++ C/hello.c
@@ -1,25 +1,25 @@
+#define P(D) 0127
 #define F(a,b) ((a)+(b),0)
 int
+#define OxB 0xB
 putchar
 (int
 _0O0O0O0O0OOO000______OOOOooOO0_
 );void W(int* s, int* t);main
-#define OxB 0xB
 (int __,char**____
-#define P(D) 0127
-){int x
+){int O00=0xFF&0,x
 [
 ]=
 {
 P(5)^31,(P(0x3)>>4)|(P(0)+9),
-P(10)+(0b1010 << ~F(11,12))|1,108,111,32,P(87),/*
+P(10)+(0b1010 << ~F(11,12))|1,108,111,(__LINE__|(~O00))<<~F(0,O00),P(87),/*
 3534,92,100,**argv,*/
 111,114,108,100
 ,P(87)/2-10},*p=x,
 *s=x;int h=0xFF&
-(97-'z'+25),O=h<<F(0[x],0xDEADBEEF+OxB);
-for(;h<12;p=s+h++-O)for(0;0;0);W(p,s);
-return! __<<(!!O);}void W(int*s,int*t)
+(97-'z'+25),OO0=h<<F(0[x],0xDEADBEEF+OxB);
+for(;h<12;p=s+h++-OO0)for(0;0;0);W(p,s);
+return! __<<(!!OO0);}void W(int*s,int*t)
 {int d=(s-t);
 if(*
 (t+11-d)

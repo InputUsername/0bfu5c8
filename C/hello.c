1ea48225c465a57e3592d0820c5b0cdb14f7dda7
InputUsername <InputUsername@users.noreply.github.com>
2015-06-14 22:20:11 +0200

    Create hello.c
    I'm not even sure how this works anymore, but it does

---
+++ C/hello.c
@@ -0,0 +1,28 @@
+#define F(a,b) ((a)+(b),0)
+#define OxB 0xB
+#define P(D) 0127
+int
+putchar
+(int
+_0O0O0O0O0OOO000______OOOOooOO0_
+);void W(int* s, int* t);main
+(int __,char**____
+){int x
+[
+]=
+{
+72,101,108,108,111,
+32,P(87),/*
+3534,92,100,**argv,*/
+111,114,108,100
+,P(87)/2-10},*p=x,
+*s=x;int h=0xFF&
+(97-'z'+25),O=h<<F(0[x],0xDEADBEEF+OxB);
+for(;h<12;p=s+h++-O)for(0;0;0);W(p,s);
+return! __<<(!!O);}void W(int*s,int*t)
+{int d=(s-t);
+if(*
+(t+11-d)
+!=0)putchar(*(t
++11-d));if (s==t)
+return;W(s-1,t);}

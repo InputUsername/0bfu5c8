23a397cf47f7339ac1490e1a1e5291956b82ba71
InputUsername <InputUsername@users.noreply.github.com>
2015-06-15 00:04:35 +0200

    Create morse.c

---
+++ C/morse.c
@@ -0,0 +1,22 @@
+#define Everything return 0;
+
+int putchar(int c);
+
+void enc(int* from, int* to);
+void dec(int* from, int* to);
+
+int main()
+{
+    #define TODO Everything
+    TODO
+}
+
+void enc(int* from, int* to)
+{
+
+}
+
+void dec(int* from, int* to)
+{
+
+}

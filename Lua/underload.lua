0c0e349127492c81b0ed93b36f3696de2306849d
Koen Bolhuis <InputUsername@users.noreply.github.com>
2015-07-31 23:39:54 +0200

    Update underload.lua
    No idea if this works but boy does it look terrible!

--- Lua/underload.lua
+++ Lua/underload.lua
@@ -1,21 +1,25 @@
-local function run(code)local _,count=string.gsub(code,"[()]","%0")if
-count%2~=0 then print("Error: unmatched parentheses")error()end local
-stack={}local function check_empty()if#stack==0 then print("Error:"..
-"empty stack")error()end end local function push(value)stack[#stack+1
-]=value end local function pop()check_empty( )local val=stack[#stack]
-stack[#stack]=nil return val end local ip=1 local n=0 while ip<=#code
-do local c=string.sub(code,ip,ip)if c=="~"then local fst,second=pop()
-,pop()push(fst)push(second)elseif c==":"then local val=pop()push(val)
-push(val)elseif c=="!"then pop()elseif c=="*"then local first,second=
-pop(),pop()push(second..first)elseif c=="("then n=1 ip=ip+1 local val
-=""while not nil do local c=string.sub(code,ip,ip)if c=="("then n=n+1
-elseif c==")"then n=n-1 end if n==0 then break end val=val..c ip=ip+1
-end push(value)elseif c=="a"then local v=pop()push("("..v..")")elseif
-c=="^"then local value=pop()code=string.sub(code,1,ip)..value..string
-.sub(code,ip+1)elseif c=="S"then local v=pop()io.write( v)end ip=ip+1
-end end if#arg<1 then print("Usage: "..arg[0].." <-i | file>\nUse -i"
-.."to enter interactive mode")error()elseif arg[1]=="-i"then while 10
-do io.write("> ")local c=io.read("*l")local r,er=pcall(run,c)if not r
-then print(err)end print()end else local fi=io.open(arg[1],"r")if not
-fi then print("Error: could not open "..arg[1])end local code=fi:read
-("*a")fi:close()run(code)print()end
+function r(p)local _,n=p:gsub("[()]","%0")if n
+%2~=0 then print"Error: unmatched parentheses"
+error()end local s={}local function e()if#s==0
+then print("Error: empty stack")error()end end
+local function u(v)s[#s+1]=v end function o()e
+()local v=s[#s]s[#s]=nil return v end	local i=
+1 n=0	while#p>=i do local c=p:sub(i,i)if "~"==
+c then a,b=o(),o()u(a)u(b)elseif":"==c then v=
+o()u(v)u(v)elseif"!"==c then o()elseif c:byte(
+)==42	local a,b=o(),o()push(b..a)elseif c=="("
+then n=1 i=i+1 local v=""while true do local c
+=string.sub(p,i,i)if c=="("then n=n+1 elseif c
+==")"then n=n-1 end if n==0 then break end v=v
+..c	i=i+1 end u(v)elseif c=="a"then	local v=o(
+)u("("..v..")")elseif c=="^"then local v=o()p=
+p:sub(1,i)..v..p:sub(i+1)elseif c=="S"then v=o
+()io.write(v)end ip=ip+1 end end a={...}if#a<1
+then print("Usage: "..a[0].." <-i | file>\nUs"
+.."e -i to enter interactive mode")error()else
+if a[1]=="-i"then	while true do	io.write("> ")
+local p=io.read("*l")local x,y=pcall(r,p)if(x)
+then print(y)end print("")end else local f=io.
+open(a[1],"r")if not f then	print("Error: cou"
+.."ld not open "..a[1])error()end	p=f:read"*a"
+f:close()r(p)print()end end

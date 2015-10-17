1c0ecf70fb33bef24adcc2399e20d4963b9415f7
Koen Bolhuis <InputUsername@users.noreply.github.com>
2015-10-17 22:11:56 +0200

    "Improved" hello.c

--- C/hello.c
+++ C/hello.c
@@ -1,5 +1,6 @@
 #define OK return;W(s-1,t);}
 #define P(D) 0127
+___$$__$____________$_________$_______$_();
 #define F(a,b) ((a)+(b),0)
 int
 #define I(A,x) !((int)x ^ (int)A)
@@ -8,10 +9,11 @@ putchar(int_0O0O0O0O0OOO000______OOOOooOO0_
 );void W(int *s,int*t);main(int __,char * *
 ____){int O00=0xF&02,x[]={P(5)^31,(P(0x3)>>
 4)|(P(0)+9),(P(&s)+(0b1010<<~F(1,2))|1)+15,
-108,111,(__LINE__-3)<<(F(O00,00)^O00),P(87)
+108,111,(__LINE__-4)<<(F(O00,00)^O00),P(87)
 ,/*353,92,100,**argv,*/111,114,108,100,P(87
 )/2-10},*p=x,* s=x;int h=0xFF&(97 -'z'+25),
 OO0=h<<F(0[ x],0xDEADBEEF+OxB);for (;h<12;p
 =s+h++-OO0)for(0;0;0);W(p,s);return! __<<(!
 !OO0);}void W(int*s,int*t){int d=(s-t);if(*
 (t+11-d)!=0)putchar(*(t+11-d));if(I(s,t))OK
+void int_main_int_argc_char$$_argv_(s,t){OK
